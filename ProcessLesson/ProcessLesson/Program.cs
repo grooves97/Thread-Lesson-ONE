7b1d7db4ef89e352a01c877dc6c56fa82d612ed8
Аслан Нурсеитов <grooves97@gmail.com>
2019-05-27 11:52:13 +0600

    Вызвали процессы calc and chrome

---
+++ ProcessLesson/ProcessLesson/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+using System.ComponentModel;
+
+namespace ProcessLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processesToKill = Process.GetProcessesByName("");
+
+            try
+            {
+                foreach (var process in processesToKill)
+                {
+                    Console.WriteLine($"{process.Id} - {process.ProcessName} - {process.SessionId}");
+                    Thread.Sleep(1000);
+                    process.Kill();
+                }
+            }
+            catch(Win32Exception exception)
+            {
+                Console.WriteLine($"Error: {exception.Message}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
